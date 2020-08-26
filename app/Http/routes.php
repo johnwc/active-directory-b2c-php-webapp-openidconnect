f461c96e50c9bd803fbce2a7441843b5002b0526
JohnWC <johnwc@users.noreply.github.com>
2020-08-25 21:35:54 -0500

    Port to AAD B2C v2 endpoints

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,9 +18,9 @@ function checkUserLoggedIn() {
 
 // Returns true if the user is an admin
 function checkUserIsAdmin() {
-	
+
 	require app_path()."/Http/Controllers/settings.php";
-	
+
 	if (!isset($_COOKIE['email'])) return false;
 	if (!in_array($_COOKIE['email'], $admins)) return false;
 	return true;
@@ -35,7 +35,7 @@ function getGivenName() {
 
 // Register for the general homepage
 Route::get('/', function() {
-	
+
 	require_once app_path()."/Http/Controllers/settings.php";
 
 	return view('blog_layout', ['user_logged_in'=>checkUserLoggedIn(),
@@ -45,15 +45,15 @@ Route::get('/', function() {
 
 // Register the homepage when an ID Token or Code is posted to the page from B2C
 Route::post('/', function () {
-	
+
 	require app_path()."/Http/Controllers/settings.php";
 	require app_path()."/Http/Controllers/TokenChecker.php";
-	
+
 	// User just signed in, must verify token/authorization code
 	if (!isset($_POST['id_token']) && !isset($_POST['code'])) {
 		return 'ERROR - only id_token or code supported';
 	}
-		
+
 	// Get state and verify it's the same as the state set in the cookie
 	$state = explode(" ", $_POST['state']);
 	$action = $state[0];
@@ -61,7 +61,7 @@ Route::post('/', function () {
 	if ($state_cookie != $_COOKIE['state']) {
 		return view('error', ['error_msg'=>"Returned state does not match cookie state"]);
 	}
-		
+
 	// Check which authorization policy was used
 	if ($action == "generic") $policy = $generic_policy;
 	if ($action == "admin") $policy = $admin_policy;
@@ -76,56 +76,56 @@ Route::post('/', function () {
 		$resp = $_POST['id_token'];
 		$resp_type = "id_token";
 	}
-	
+
 	// Verify token
 	$tokenChecker = new TokenChecker($resp, $resp_type, $clientID, $client_secret, $policy);
 	$verified = $tokenChecker->authenticate();
 	if ($verified == false) {
 		return view('error', ['error_msg'=>"Token validation error"]);
 	}
-		
-	// Fetch user's email and check if admin 
+
+	// Fetch user's email and check if admin
 	$email = $tokenChecker->getClaim("emails");
 	$email = $email[0];
 	if (in_array($email, $admins)) {
-		$acr = $tokenChecker->getClaim("acr");
-			
+		$tfp = $tokenChecker->getClaim("tfp");
+
 		// If user did not authenticate with admin_policy, redirect to admin policy
-		if ($acr != $admin_policy) {
+		if ($tfp != $admin_policy) {
 			$endpoint_handler = new EndpointHandler($admin_policy);
 			$authorization_endpoint = $endpoint_handler->getAuthorizationEndpoint()."&state=admin";
-			
+
 			// Set cookie for state
 			$state = rand();
 			setcookie("state", $state);
 			$authorization_endpoint = $authorization_endpoint . "+" . $state;
-						
+
 			// Redirect to sign up/sign in page for admins
 			header('Location: '.$authorization_endpoint);
-		}	
+		}
 	}
-		
+
 	// Set cookies
 	setcookie("email", $email);
 	$given_name = $tokenChecker->getClaim("given_name");
 	setcookie("user", $given_name);
-			
-	// Render homepage view	
+
+	// Render homepage view
 	return view('blog_layout', ['user_logged_in'=>true,
 						'user_is_admin'=>in_array($email, $admins),
 						'given_name'=>$given_name]);
-		
+
 });
 
 // Register the login route
 Route::get('/login', function () {
     require app_path()."/Http/Controllers/settings.php";
 	require app_path()."/Http/Controllers/EndpointHandler.php";
-	
+
 	// Set cookie for state
 	$state = rand();
 	setcookie("state", $state);
-	
+
 	// Redirect to sign up/sign in page
 	$endpoint_handler = new EndpointHandler($generic_policy);
 	$authorization_endpoint = $endpoint_handler->getAuthorizationEndpoint()."&state=generic"."+". $state;
@@ -134,7 +134,7 @@ Route::get('/login', function () {
 
 // Register the logout route
 Route::get('/logout', function () {
-	
+
 	// User not authenticated
 	if (!isset($_POST['id_token']) && !isset($_POST['code']) && !isset($_COOKIE['user'])) {
 		return view('error', ['error_msg'=>'Trying to log out when not logged in']);
@@ -142,27 +142,27 @@ Route::get('/logout', function () {
 	else {
 		require app_path()."/Http/Controllers/settings.php";
 		require app_path()."/Http/Controllers/EndpointHandler.php";
-		
+
 		// Delete cookies
 		setcookie("user", "", time() - 3600);
 		setcookie("email", "", time() - 3600);
-		
+
 		// Redirect to logout page
 		$signout_endpoint_handler = new EndpointHandler($generic_policy);
 		$signout_uri = $signout_endpoint_handler->getEndSessionEndpoint();
 		return redirect($signout_uri);
-	} 
+	}
 });
 
 // Register the edit profile route
 Route::get('/edit_profile', function () {
     require app_path()."/Http/Controllers/settings.php";
 	require app_path()."/Http/Controllers/EndpointHandler.php";
-	
+
 	// Set cookie for state
 	$state = rand();
 	setcookie("state", $state);
-	
+
 	// Redirect to sign up/sign in page
 	$endpoint_handler = new EndpointHandler($edit_profile_policy);
 	$authorization_endpoint = $endpoint_handler->getAuthorizationEndpoint()."&state=edit_profile"."+".$state;
